cc9e711bd268edc66caf3937919459e8054a9871
moguy93 <guymorgan@live.fr>
2023-09-04 15:21:58 +0200

    add exercise 1.1

---
+++ corrections/ex01Correction.py
@@ -0,0 +1,31 @@
+# Écrire une boucle Python qui affiche les entiers de 1 à 10 avec une boucle while
+
+def afficher_entiers_while():
+    i = 1
+    while i <= 10:
+        print(i)
+        i += 1
+    print('\n')
+
+# Écrire une boucle Python qui affiche les entiers de 1 à 10 avec une boucle for
+
+def afficher_entiers_for():
+    for i in range(1, 11, 2):
+        print(i)
+    print('\n')
+
+# Fonction pour afficher le contenu d'un tableau d'entiers
+def afficher_tableau(tableau):
+    for element in tableau:
+        print(f'{element}\n')
+        
+# Exemple d'utilisation des fonctions
+def exemple():
+    entiers = [23, 45, 6, 89, 0]
+    mots = ["hi", "bonjour", "hello"]
+    afficher_entiers_while()
+    afficher_entiers_for()
+    afficher_tableau(entiers)
+    afficher_tableau(mots)
+    
+exemple()
