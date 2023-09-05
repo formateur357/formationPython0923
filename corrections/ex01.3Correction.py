82d8164559e6dfdac30d63137e56cae188f17739
moguy93 <guymorgan@live.fr>
2023-09-05 12:21:50 +0200

    uodate correction 01.3

--- corrections/ex01.3Correction.py
+++ corrections/ex01.3Correction.py
@@ -1,14 +1,44 @@
-from itertools import product
+# from itertools import product
 
-# Définir un dictionnaire avec des clés et des listes de lettres.
-donnees = {'1': ['a', 'b'], '2': ['c', 'd']}
+# # Définir un dictionnaire avec des clés et des listes de lettres.
+# donnees = {'1': ['a', 'b'], '2': ['c', 'd']}
 
-# Créer une liste de listes contenant les lettres pour chaque clé.
-listes_de_lettres = [donnees[key] for key in donnees]
+# # Créer une liste de listes contenant les lettres pour chaque clé.
+# listes_de_lettres = [donnees[key] for key in donnees]
 
-# Utiliser la fonction product pour générer toutes les combinaisons possibles.
-combinaisons = list(product(*listes_de_lettres))
+# # Utiliser la fonction product pour générer toutes les combinaisons possibles.
+# combinaisons = list(product(*listes_de_lettres))
 
-# Afficher les combinaisons.
-for combinaison in combinaisons:
-    print(''.join(combinaison))
+# # Afficher les combinaisons.
+# for combinaison in combinaisons:
+#     print(''.join(combinaison))
+
+
+
+#==============================================================================
+#Solution sans itertools
+
+def generate_combinations(data, current_key_index=0, current_combination=[]):
+    if current_key_index == len(data):
+        # If we have processed all keys, print the combination
+        print(''.join(current_combination))
+        return
+
+    current_key = list(data.keys())[current_key_index]
+    letters_for_key = data[current_key]
+
+    for letter in letters_for_key:
+        # Add the current letter to the combination
+        current_combination.append(letter)
+
+        # Recursively generate combinations for the next key
+        generate_combinations(data, current_key_index + 1, current_combination)
+
+        # Remove the last letter to backtrack and try the next letter
+        current_combination.pop()
+
+# Sample data: dictionary with keys and lists of letters
+data = {'1':['a','b'], '2':['c','d'], '3':['e','f']}
+
+# Start generating combinations
+generate_combinations(data)
