359b9ff05c066baff12a6cb034fabb5418844943
moguy93 <guymorgan@live.fr>
2023-09-05 14:24:26 +0200

    add exo

---
+++ corrections/ex02.1Correction.py
@@ -0,0 +1,9 @@
+# Fonction pour calculer la moyenne d'âge des étudiants et trouver l'étudiant avec la meilleure moyenne.
+        # Ouvrir le fichier d'entrée en mode lecture.
+            # Ignorer la première ligne (en-tête).
+                # Calculer la moyenne d'âge.
+                # Vérifier si cette moyenne est la meilleure jusqu'à présent.
+            # Calculer la moyenne d'âge.
+            # Ouvrir le fichier de sortie en mode écriture.
+
+# Appel de la fonction principale.
---
+++ corrections/ex02Correction.py
@@ -0,0 +1,23 @@
+# 1
+
+# Ouvrir le fichier en mode lecture
+
+# Ouvrir le fichier en mode écriture et ajouter des commentaires
+        # Ajouter un commentaire à la première ligne
+        
+# Fermer le fichier
+
+
+#2
+
+# Ouvrir le fichier en mode lecture
+
+# Calculer le nombre de lignes
+
+# Calculer le nombre de mots
+
+# Calculer le nombre de caractères
+
+# Afficher les résultats
+
+# Fermer le fichier
