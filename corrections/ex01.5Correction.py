c6cd880be499a835563df1ef5025a39e2e30f733
moguy93 <guymorgan@live.fr>
2023-09-05 12:26:08 +0200

    add correction exo5

--- corrections/ex01.5Correction.py
+++ corrections/ex01.5Correction.py
@@ -1,4 +1,12 @@
 # Définir une fonction pour élever un nombre au carré
+def carre(x):
+    return x ** 2
+
 # Créer une liste de nombres
+nombres = [1, 2, 3, 4, 5]
 # Utiliser la fonction map() pour élever au carré chaque élément de la liste
+nombre_carres = list(map(carre, nombres))
+
 # Afficher les nombres au carré
+print("liste d'origine :", nombres)
+print("liste au carre :", nombre_carres)
