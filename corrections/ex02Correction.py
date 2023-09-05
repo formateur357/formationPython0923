fec9243e12cf2f71d3ece0b12949dd5509b60f72
moguy93 <guymorgan@live.fr>
2023-09-05 16:00:52 +0200

    add correction

--- corrections/ex02Correction.py
+++ corrections/ex02Correction.py
@@ -1,23 +1,35 @@
 # 1
 
 # Ouvrir le fichier en mode lecture
+# fichier = open("text.txt", "r")
+# fichier.close()
 
-# Ouvrir le fichier en mode écriture et ajouter des commentaires
-        # Ajouter un commentaire à la première ligne
-        
-# Fermer le fichier
+# with open("/Users/morganguy/Formation_Workspace/formationPython0923/corrections/text.txt", "r") as fichier:
+#     lignes = fichier.readlines()
 
+# # Ouvrir le fichier en mode écriture et ajouter des commentaires
+# with open("/Users/morganguy/Formation_Workspace/formationPython0923/corrections/text.txt", "w") as fichier:
+#     for i, ligne in enumerate(lignes):
+#         # Ajouter un commentaire à chaque ligne
+#         fichier.write(ligne.strip() + "\n# Commentaire de ligne\n")
+  
 
 #2
 
 # Ouvrir le fichier en mode lecture
+with open("/Users/morganguy/Formation_Workspace/formationPython0923/corrections/text.txt", "r") as fichier:
+    contenu = fichier.read()
 
 # Calculer le nombre de lignes
+nb_lignes = len(contenu.splitlines())
 
 # Calculer le nombre de mots
+nb_mots = len(contenu.split())
 
 # Calculer le nombre de caractères
+nb_char = len(contenu)
 
 # Afficher les résultats
-
-# Fermer le fichier
+print("Nombre de lignes:", nb_lignes)
+print("Nombre de mots:", nb_mots)
+print("Nombre de caractères:", nb_char)
