0aef2bae060fc0ad72ff8dfa4ffc71e0a1f8b8e4
moguy93 <guymorgan@live.fr>
2023-09-05 12:18:18 +0200

    add correction

---
+++ corrections/ex01.5Correction.py
@@ -0,0 +1,4 @@
+# Définir une fonction pour élever un nombre au carré
+# Créer une liste de nombres
+# Utiliser la fonction map() pour élever au carré chaque élément de la liste
+# Afficher les nombres au carré
