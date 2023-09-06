97a4c01c9332f5144231866eff36972fbd236cbc
johanessian <jerome.ohanessian@docaposte.fr>
2023-09-06 10:58:07 +0200

    à corriger

--- rendu/Jerome/exo03.3.py
+++ rendu/Jerome/exo03.3.py
@@ -2,12 +2,12 @@ stock = {}
 
 try:
     while True:
-            nom_voiture = input("entrez le nom de la voiture : ")
-            quantite = float(input("Entrez la quantité (+ pour ajouter, - pour réduire : "))
-            if nom_voiture in stock:
-                 stock[nom_voiture] += quantite
-            else:
-                stock[nom_voiture] = quantite
+        nom_voiture = input("entrez le nom de la voiture : ")
+        quantite = float(input("Entrez la quantité (+ pour ajouter, - pour réduire : "))
+        if nom_voiture in stock:
+            stock[nom_voiture] += quantite
+        else:
+            stock[nom_voiture] = quantite
 except EOFError:
         pass
 
