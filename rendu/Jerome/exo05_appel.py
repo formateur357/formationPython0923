abcb178989492fd7df5bca2c2374ffe8c7732e77
johanessian <jerome.ohanessian@docaposte.fr>
2023-09-06 14:59:26 +0200

    joh1

--- rendu/Jerome/exo05_appel.py
+++ rendu/Jerome/exo05_appel.py
@@ -8,9 +8,11 @@ restaurant.add_item_to_menu("Falafel", 14)
 
 restaurant.book_table(1, "Pierre", "20:00")
 restaurant.book_table(2, "Paul", "20:30")
+restaurant.book_table(2, "Jacques", "21:00")
 
 restaurant.customer_order("Pierre", ["Falafel", "Pâtes"])
 restaurant.customer_order("Paul", ["Côte de Boeuf"])
+restaurant.customer_order("Jacques", ["Falafel"])
 
 restaurant.print_menu()
 restaurant.print_table_reservations()
