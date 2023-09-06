e6206da32144ac5fe8bef09ac2fecea82d0feb91
mattarFormation <144007645+mattarFormation@users.noreply.github.com>
2023-09-06 11:26:27 +0200

    Exercice 3_1

--- rendu/Mohamed/exo3_1.py
+++ rendu/Mohamed/exo3_1.py
@@ -6,14 +6,17 @@ def calcul_moyenne():
     try:
       while True:
         try:
-            nombre=int(input("Veuillez entrer un nombre :"))
+            nombre=int(input("Veuillez entrer un nombre (Ctrl-c ou Ctrl-d pour terminer):"))
             stock_nombre.append(nombre)
         except ValueError:
             print("Merci d'entrer un nombre !")
             continue
     except KeyboardInterrupt:
-            moyenne = sum(stock_nombre)/len(stock_nombre)
-            print(f"\nla moyenne est de {moyenne}\n")
+        pass
+    except EOFError:
+        pass
+    moyenne = sum(stock_nombre)/len(stock_nombre)
+    print(f"\nla moyenne est de {moyenne}\n")
 
 #calcul_moyenne()
 
@@ -22,16 +25,19 @@ def trie_mots():
     try:
         while True:
             try:
-                nombre=str(input("Veuillez entrer un mot :"))
+                nombre=str(input("Veuillez entrer un mot (Ctrl-c ou Ctrl-d pour terminer) :"))
                 stock_mots.append(nombre)
             except ValueError:
                 print("Merci d'entrer un nombre !")
                 continue
     except KeyboardInterrupt:
-            stock_mots.sort()
-            print(f"\nOrdre croissant : {stock_mots}\n")
-            stock_mots.sort(reverse=True)
-            print(f"\nOrdre décroissant : {stock_mots}\n")
+        pass
+    except EOFError:
+        pass
+    stock_mots.sort()
+    print(f"\nOrdre croissant : {stock_mots}\n")
+    stock_mots.sort(reverse=True)
+    print(f"\nOrdre décroissant : {stock_mots}\n")
 #trie_mots()
 
 def calcul_stock(stock):
@@ -48,9 +54,8 @@ def stock_voitures():
     try:
         while True:
             try:
-                voiture=str(input("Veuillez entrer la marque du véhicule :"))
-                keys = list(stock_voiture.keys())
-                if voiture in keys:
+                voiture=str(input("Veuillez entrer la marque du véhicule (Ctrl-c ou Ctrl-d pour terminer) :"))
+                if voiture in stock_voiture:
                     quantite=str(input("Nombre a défalquer du stock ex(-1 ou +1) :"))
                     x = re.search("(-)([0-9])+|(\+)([0-9]+)", quantite)
                     if x.group()[0] == "-":
@@ -66,6 +71,9 @@ def stock_voitures():
                 print("Merci d'entrer une chaine de str !")
                 continue
     except KeyboardInterrupt:
-            print(f"\nStock voiture : {stock_voiture}\n")
-            calcul_stock(stock_voiture)
+        pass
+    except EOFError:
+        pass
+    print(f"\nStock voiture : {stock_voiture}\n")
+    calcul_stock(stock_voiture)
 stock_voitures()
