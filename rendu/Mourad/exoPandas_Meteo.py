cb06a7f1f23af41837dc929b1e3fe401db30aff1
Mourad-Charfi <mourad.charfi.ing@gmail.com>
2023-09-07 12:44:35 +0200

    maj

--- rendu/Mourad/exoPandas_Meteo.py
+++ rendu/Mourad/exoPandas_Meteo.py
@@ -3,9 +3,16 @@ import pandas as pd
 data = pd.read_json('C:/Formation_Python/meteo.json')
 #print(data.head(5))
 df=pd.json_normalize(data["results"])
-filter_temp=df['temp'].notnull()
-resultat=df[filter_temp]
+Resultat = pd.DataFrame(df)
+print(Resultat.head(5))
+#  nouvelle colonne "temp_moy" 
+Resultat["temp_moy"]=(Resultat['temp_min'] + Resultat['temp_max'])/2
+print(Resultat)
+# temperature max" 
+print(Resultat.loc[:, ["temp"]].max())
+# temperature min" 
+print(Resultat.loc[:, ["temp"]].min())
+# humidité moyenne
+print(Resultat.loc[:, ["humidity"]].mean())
 
-#Calculez la température moyenne quotidienne en ajoutant une nouvelle colonne "temp_moy" au DataFrame.
-print(resultat)
 
