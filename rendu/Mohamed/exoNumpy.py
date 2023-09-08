7710e5f148e825ba4d1f3c6491df1a2a35beff90
mattarFormation <144007645+mattarFormation@users.noreply.github.com>
2023-09-08 11:42:00 +0200

    Exercice Numpy 1

---
+++ rendu/Mohamed/exoNumpy.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+tableau = np.arange(1, 11)
+print(tableau)
+
+# Multiplication par  de 2
+print("\n#########Mutliplication par fonction #######\n")
+fTableau = np.multiply(tableau, 2)
+print(fTableau)
+
+print("\n#########Mutliplication par chiffre #######\n")
+mTableau = np.array(tableau)*2
+print(mTableau)
+
+print("\n#########Somme #######\n")
+sumTableau = np.sum(tableau)
+print(sumTableau)
+
+print("\n#########Chiffre paire #######\n")
+paire = np.where(tableau % 2 != 0)
+print(paire)
+
+print("\n#########Chiffre superieur 5 #######\n")
+sup5 = np.where(tableau > 5)
+print(sup5)
+
+print("\n#########Creation tableau 2d #######\n")
+nTableau = np.arange(1, 28).reshape(3, 9)
+print(nTableau)
+
+print("\n#########Ligne sum #######\n")
+print(np.sum(nTableau, axis=1))
+
+print("\n#########Colonne sum #######\n")
+print(np.sum(nTableau, axis=0))
+
+print("\n#########Remodeler #######\n")
+rTableau = np.reshape(nTableau, (27,))
+
+aTableau = np.append(rTableau, [28, 29, 30])
+print(aTableau)
