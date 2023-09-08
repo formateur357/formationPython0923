af8e67eaea901da64b7efb7156ec565a5955b66f
mattarFormation <144007645+mattarFormation@users.noreply.github.com>
2023-09-08 13:05:16 +0200

    Exercice Numpy 2

--- rendu/Mohamed/exoNumpy_2.py
+++ rendu/Mohamed/exoNumpy_2.py
@@ -16,9 +16,8 @@ print(tableau.argmax())
 
 print("\n########## Tri tableau ############\n")
 sort = np.sort(tableau)
-print(np.sort(tableau))
-
-print(f"\nDécroissant \n {sort[::-1].argsort()}")
+print(f"\nCroissant \n {sort}\n")
+print(f"\nDécroissant \n {sort[::-1].argsort()}\n")
 
 print("\n########## Matrice ############\n")
 a = np.random.randint(1, 11, (4, 4))
@@ -28,12 +27,9 @@ print(trans)
 
 print("\n########## Filtrage ############\n")
 b = np.random.rand(100)
-supp = np.where(b > 0.5, b, "")
-inf = np.where(b < 0.7, b, "")
-print(f"Superieur 0.5 : {supp}\n")
-print(f"Inferieur 0.7 : {inf}\n")
+filtre = np.where(b > 0.5, np.where(b < 0.7, b, 0), 0)
+print(filtre)
 
 print("\n########## Remplacement de donnée ############\n")
-b = np.where(b < 0.2, 0, b)
-b = np.where(b > 0.8, 1, b)
+b = np.where(b < 0.2, 0, np.where(b > 0.8, 1, b))
 print(b)
